0ee930f8a94a2bb14e37441432d6d8de530d4efd
usr2486 <usr2486@salesianos-lacuesta.net>
2025-06-11 11:48:59 +0100

    Creacion del proyecto

---
+++ PROG-UD7-Ficheritos/src/net/salesianos/actividad1/main.java
@@ -0,0 +1,7 @@
+package net.salesianos.actividad1;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Escribeme aqui algo pero sin pasarte colega");
+    }
+}
