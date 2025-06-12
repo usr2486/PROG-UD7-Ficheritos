fc46a2cec21f426c1739ffed3f1ff20b728b7935
usr2486 <usr2486@salesianos-lacuesta.net>
2025-06-12 16:44:33 +0100

    Actividad 2 completada

---
+++ PROG-UD7-Ficheritos/src/net/salesianos/actividad2/main.java
@@ -0,0 +1,20 @@
+package net.salesianos.actividad2;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class main {
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("ficherito.txt");
+            int code;
+            while ((code = fileReader.read()) != -1) {
+                char character = (char) code;
+                System.out.print(character + "_" + code + ", ");
+            }
+            fileReader.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
