3397e9cd0a47b4561f34989d1a97526d1d14fd66
usr2486 <usr2486@salesianos-lacuesta.net>
2025-06-12 16:27:49 +0100

    Programa act1 funcional.

--- PROG-UD7-Ficheritos/src/net/salesianos/actividad1/main.java
+++ PROG-UD7-Ficheritos/src/net/salesianos/actividad1/main.java
@@ -1,10 +1,22 @@
 package net.salesianos.actividad1;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        FileWriter fileWriter = null;
+
+        try {
+            fileWriter = new FileWriter("texto.txt", true);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+            scanner.close();
+            return;
+        }
+
         System.out.println("Escribeme aqui algo pero sin pasarte colega");
 
         String text = scanner.nextLine();
@@ -13,7 +25,25 @@ public class main {
 
             System.out.println("Eso es muy corto colega explayate mas");
             text = scanner.nextLine();
+
         }
+        String formattedText = text.toUpperCase().replaceAll(" ", "_");
+        System.out.println("Tu texto es: " + formattedText);
+
+        try {
+            fileWriter.write(formattedText);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+
         scanner.close();
+
+        if (fileWriter != null) {
+            try {
+                fileWriter.close();
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
